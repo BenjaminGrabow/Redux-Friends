f4131b24746ac1ddc4e614930cb3e82edfe7c1e9
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-20 16:16:16 +0200

    Feature: added slider for item list

--- friends/src/Components/Friends/Friends.js
+++ friends/src/Components/Friends/Friends.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetch, deleter, add, update } from '../../store/actions';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
@@ -24,128 +26,130 @@ margin: 1rem;
 `;
 
 class Friends extends React.Component {
-        constructor(props) {
-                super(props);
-                this.state = {
-                        name: '',
-                        age: '',
-                        email: '',
-                        input: 'off',
-                        nameUpdate: '',
-                        ageUpdate: '',
-                        emailUpdate: ''
-                }
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      age: '',
+      email: '',
+      input: 'off',
+      nameUpdate: '',
+      ageUpdate: '',
+      emailUpdate: ''
+    }
+  }
 
-        componentDidMount = () => {
-                this.props.fetch();
-        };
+  componentDidMount = () => {
+    this.props.fetch();
+  };
 
-        handleChange = (e) => {
-                this.setState({
-                        [e.target.name]: e.target.value
-                });
-        };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-        showInput = () => {
-                this.setState({
-                        input: 'on'
-                });
-        };
+  showInput = () => {
+    this.setState({
+      input: 'on'
+    });
+  };
 
-        updateIt = (id) => {
-                this.props.update(id, this.state.nameUpdate, this.state.ageUpdate, this.state.emailUpdate);
+  updateIt = (id) => {
+    this.props.update(id, this.state.nameUpdate, this.state.ageUpdate, this.state.emailUpdate);
 
 
-                this.setState({
-                        nameUpdate: '',
-                        ageUpdate: '',
-                        emailUpdate: '',
-                        input: 'off'
-                });
-        };
+    this.setState({
+      nameUpdate: '',
+      ageUpdate: '',
+      emailUpdate: '',
+      input: 'off'
+    });
+  };
 
-        adder = () => {
-                this.props.add(this.state.name, this.state.age, this.state.email);
+  adder = () => {
+    this.props.add(this.state.name, this.state.age, this.state.email);
 
-                this.setState({
-                        name: '',
-                        age: '',
-                        email: ''
-                });
-        };
+    this.setState({
+      name: '',
+      age: '',
+      email: ''
+    });
+  };
 
-        render() {
-                return (
-                        <StyledDiv>
-                                {this.props.friends.map(friend => {
-                                        return <div
-                                                className="friend"
-                                                key={friend.id}>
-                                                <h1>{friend.name}</h1>
-                                                <p>{friend.age}</p>
-                                                <p>{friend.email}</p>
-                                                <button onClick={() =>
-                                                        this.props.deleter(friend.id)}>
-                                                        Delete
+  render() {
+    return (
+      <StyledDiv>
+        <Slider>
+        {this.props.friends.map(friend => {
+          return <div
+            className="friend"
+            key={friend.id}>
+            <h1>{friend.name}</h1>
+            <p>{friend.age}</p>
+            <p>{friend.email}</p>
+            <button onClick={() =>
+              this.props.deleter(friend.id)}>
+              Delete
                                                                  </button>
-                                                <button onClick={
-                                                        this.showInput}>
-                                                        Update
+            <button onClick={
+              this.showInput}>
+              Update
                                                                    </button>
-                                                <div
-                                                        className={this.state.input === 'off' ? 'off' : null}>
-                                                        <input
-                                                                onChange={this.handleChange}
-                                                                name="nameUpdate"
-                                                                type="text"
-                                                                value={this.state.nameUpdate} />
-                                                        <input
-                                                                onChange={this.handleChange}
-                                                                name="ageUpdate"
-                                                                type="number"
-                                                                value={this.state.ageUpdate} />
-                                                        <input
-                                                                onChange={this.handleChange}
-                                                                name="emailUpdate"
-                                                                type="text"
-                                                                value={this.state.emailUpdate} />
-                                                        <button onClick={() =>
-                                                                this.updateIt(friend.id)}>
-                                                                update
+            <div
+              className={this.state.input === 'off' ? 'off' : null}>
+              <input
+                onChange={this.handleChange}
+                name="nameUpdate"
+                type="text"
+                value={this.state.nameUpdate} />
+              <input
+                onChange={this.handleChange}
+                name="ageUpdate"
+                type="number"
+                value={this.state.ageUpdate} />
+              <input
+                onChange={this.handleChange}
+                name="emailUpdate"
+                type="text"
+                value={this.state.emailUpdate} />
+              <button onClick={() =>
+                this.updateIt(friend.id)}>
+                update
                                                                    </button>
-                                                </div>
-                                        </div>
-                                })}
-                                <input
-                                        onChange={this.handleChange}
-                                        name="name"
-                                        type="text"
-                                        value={this.state.name} />
-                                <input
-                                        onChange={this.handleChange}
-                                        name="age"
-                                        type="number"
-                                        value={this.state.age} />
-                                <input
-                                        onChange={this.handleChange}
-                                        name="email"
-                                        type="text"
-                                        value={this.state.email} />
-                                <button onClick={this.adder} >
-                                        Add
+            </div>
+          </div>
+        })}
+          </Slider>
+        <input
+          onChange={this.handleChange}
+          name="name"
+          type="text"
+          value={this.state.name} />
+        <input
+          onChange={this.handleChange}
+          name="age"
+          type="number"
+          value={this.state.age} />
+        <input
+          onChange={this.handleChange}
+          name="email"
+          type="text"
+          value={this.state.email} />
+        <button onClick={this.adder} >
+          Add
                                            </button>
-                        </StyledDiv>
-                );
-        }
+      </StyledDiv>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-        return {
-                friends: state.friends,
-                loading: state.loading,
-                error: state.error
-        }
+  return {
+    friends: state.friends,
+    loading: state.loading,
+    error: state.error
+  }
 };
 
 export default connect(mapStateToProps, { fetch, deleter, add, update })(Friends);
